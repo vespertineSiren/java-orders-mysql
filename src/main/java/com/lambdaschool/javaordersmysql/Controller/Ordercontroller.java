32aa6bd026bf3994de9b1d0cb092202fb094a208
Patrick Martin <patrickmartin88@gmail.com>
2019-02-25 16:11:11 -0800

    Added a few endpoints before finalizing.

--- src/main/java/com/lambdaschool/javaordersmysql/Controller/Ordercontroller.java
+++ src/main/java/com/lambdaschool/javaordersmysql/Controller/Ordercontroller.java
@@ -1,10 +1,15 @@
 package com.lambdaschool.javaordersmysql.Controller;
 
+import com.lambdaschool.javaordersmysql.Model.Order;
 import com.lambdaschool.javaordersmysql.Repository.Orderrepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value ="/orders", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -12,4 +17,14 @@ public class Ordercontroller {
 
     @Autowired
     Orderrepository orderrepository;
+
+    @GetMapping("/orders")
+    public List<Order> listAllOrders(){
+        return orderrepository.findAll();
+    }
+
+    @GetMapping("/ordnum/{ordnum}")
+    public Order findOrderbyOrderNumber(@PathVariable long ordnum){
+        return orderrepository.findById(ordnum).get();
+    }
 }
